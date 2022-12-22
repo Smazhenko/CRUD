b51b5825baff3bf09c9e2fd254aa30f2fe8e9268
Vitaliy <VitaliyZhukovplz@gmail.com>
2022-12-22 11:52:46 +0200

    create h_to_p

---
+++ migrations/20221222094705-heroes_to_powers.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  
+     await queryInterface.createTable('heroes_to_powers', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      heroId: {
+        field: 'hero_id',
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'heroes',
+            key: 'id'
+          },
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      powerId: {
+        field: 'power_id',
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'superpowers',
+            key: 'id'
+          }
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+      });
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+     await queryInterface.dropTable('heroes_to_powers');
+     
+  }
+};
