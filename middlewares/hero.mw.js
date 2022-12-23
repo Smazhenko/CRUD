00d40e2b5bd8032c540fc2c12887f8aac39b832b
Vitaliy <VitaliyZhukovplz@gmail.com>
2022-12-23 15:20:15 +0200

    hero mw redacted

---
+++ middlewares/hero.mw.js
@@ -0,0 +1,31 @@
+const {Hero} = require('../models');
+
+const {HERO_SCHEMA} = require('../chemas/hero.schema');
+
+module.exports.validateHero = async (req, res, next) =>{
+    try{
+        const validated = await HERO_SCHEMA.validate(req.body);
+        if(validated) {
+            next()
+        }
+    } catch(err) {
+        next(err);
+    }
+}
+
+
+module.exports.getHeroInstance  = async( req, res, next) =>{
+    try{
+        const {params:{heroId}} = req;
+        const hero = await Hero.findByPk(heroId, {
+});
+        if(!hero) {
+            throw new Error ('Hero not found');
+        }
+        req.heroInstance = hero;
+        next();
+
+    } catch(err) {
+        next(err);
+    }
+}
