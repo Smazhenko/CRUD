0a224173413a730b256eb6cbb4201e62d0315a96
Vitaliy <VitaliyZhukovplz@gmail.com>
2022-12-23 15:19:06 +0200

    cretae hero schema

---
+++ chemas/hero.schema.js
@@ -0,0 +1,8 @@
+const yup = require('yup');
+
+module.exports.HERO_SCHEMA = yup.object({
+    nickName: yup.string().required('Nick Name is required').min(1),
+    realName: yup.string().required('Real Name is required').min(1),
+    originDescription: yup.string(),
+    cathPhrase:yup.string()
+})
